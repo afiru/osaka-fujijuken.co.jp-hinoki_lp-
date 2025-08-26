2b289534311118a27becf9cc6bbc5cb25b2a9d4c
Miho Kinoshita <afiru.pain@gmail.com>
2025-08-26 15:27:54 +0900

    HOMEURL変更

--- config.php
+++ config.php
@@ -1,14 +1,12 @@
 <?php
 $url = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://') . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-if(strpos($url,'localhost')){
+if (strpos($url, 'localhost')) {
     $homeUrl = "http://localhost/anken/test/lp/";
-    $imgUrl = $homeUrl."/img/";
-}
-elseif(strpos($url,'sunplads-staging.com')){
-    $homeUrl = "https://sunplads-staging.com/test_up/";
-    $imgUrl = $homeUrl."/img/";
-}
-else {
+    $imgUrl = $homeUrl . "/img/";
+} elseif (strpos($url, 'sunplads-staging.com')) {
+    $homeUrl = "https://sunplads-staging.com/test_up/osaka/fujijuken.co.jp/hinoki_lp/";
+    $imgUrl = $homeUrl . "/img/";
+} else {
     $homeUrl = "";
-    $imgUrl = $homeUrl."/img/";
+    $imgUrl = $homeUrl . "/img/";
 }
